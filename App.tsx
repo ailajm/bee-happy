9a39de5fbaf4e33c3ee655c81d7122ce6424c818
cotymckinney <cotymckinney@outlook.com>
2023-09-03 03:39:18 -0400

    Moved affirmations to their own module.  Sourcing from API on roadmap.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 // Imports
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { Affirmations } from './Affirmation';
 
 const App = () => {
   // Test affirmations
-  const affirmations: string[] = [
-    "I am strong.",
-    "I am capable.",
-    "I am worthy of success.",
-    "I am worthy of happiness.",
-    "I am becoming the best version of myself.",
-    "I believe in my ability to overcome challenges.",
-    "I am grateful for the positive things in my life."
-  ];
+  const affirmations: string[] = Affirmations;
 
   // State
   const [currentAffirmation, setCurrentAffirmation] = useState<string>('');
