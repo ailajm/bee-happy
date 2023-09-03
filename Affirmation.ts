d5f67bef8c0aa815a1a3cc61ce0a3d2c5381ae9c
cotymckinney <cotymckinney@outlook.com>
2023-09-03 05:03:08 -0400

    Renamed

--- Affirmation.ts
+++
@@ -1,9 +0,0 @@
-export const Affirmations: string[] = [
-  'I am strong.',
-  'I am capable.',
-  'I am worthy of success.',
-  'I am worthy of happiness.',
-  'I am becoming the best version of myself.',
-  'I believe in my ability to overcome challenges.',
-  'I am grateful for the positive things in my life.',
-];
